0c3032a5d4ec3866dbeb1c165542ea8f6b8f025b
denisS <densproatem@example.com>
2024-04-21 08:57:33 -0400

    Implemented custom problem details factory and via error endpoint

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,5 +1,9 @@
+// using BuberDinner.Api.Filters;
+// using BuberDinner.Api.Middleware;
+using BuberDinner.Api.Errors;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -8,19 +12,24 @@ var builder = WebApplication.CreateBuilder(args);
                   .AddInfrastructure(builder.Configuration);
   // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
   builder.Services.AddEndpointsApiExplorer();
-  builder.Services.AddSwaggerGen();
+  // builder.Services.AddSwaggerGen();
   builder.Services.AddControllers();
+  builder.Services.AddSingleton<ProblemDetailsFactory, BubberDinnerProblemDetailsFactory>();
+  // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 }
 
 var app = builder.Build();
 {
   // Configure the HTTP request pipeline.
-  if (app.Environment.IsDevelopment())
-  {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-  }
+  // if (app.Environment.IsDevelopment())
+  // {
+  //   app.UseSwagger();
+  //   app.UseSwaggerUI();
+  // }
+  // app.UseMiddleware<ErrorHandlingMiddleware>();
   app.UseHttpsRedirection();
+  app.UseExceptionHandler("/error");
+
   // Map the controller endpoints
   app.MapControllers();
   app.Run();
